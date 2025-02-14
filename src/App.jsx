d9980090d486fc5c2542e0e4e9934424bac57a9a
hudo-ng <hmdong901026@gmail.com>
2025-02-14 07:09:04 -0700

    getting route data using useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import SearchParams from "./SearchParams";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt me!</h1>
+      <header>
+        <Link to="/">Adopt me</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
