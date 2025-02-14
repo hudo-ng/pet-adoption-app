fd9ee2342ae85f6cef405df7c5a66eb0375d5e44
hudo-ng <hmdong901026@gmail.com>
2025-02-13 23:43:12 -0700

    refactor SearchParams into smaller components

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,9 +1,19 @@
-export default function Pet(props) {
+export default function Pet({ name, animal, images, breed, id, location }) {
+  let hero = "https://petsimages.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
   );
 }
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -73,14 +73,7 @@ const SearchParams = () => {
         </select>
         <button type="submit">Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
