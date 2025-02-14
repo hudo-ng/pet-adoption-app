f1bbdfcf56a82005cce388116a67909bdf8eacb5
hudo-ng <hmdong901026@gmail.com>
2025-02-13 22:57:08 -0700

    create custom hook to load animal breed list

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -8,8 +9,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-
-  const BREEDS = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -64,10 +64,10 @@ const SearchParams = () => {
           onChange={(e) => {
             setBreed(e.target.value);
           }}
-          disabled={BREEDS.length === 0}
+          disabled={breeds.length === 0}
         >
           <option></option>
-          {BREEDS.map((breed) => (
+          {breeds.map((breed) => (
             <option value={breed}>{breed}</option>
           ))}
         </select>
